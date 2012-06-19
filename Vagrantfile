0c082fcae491ad5621fccbe2ff2e87af9790ca9f
Victor Ng <victor@crankycoder.com>
2012-06-19 02:41:54 -0400

    patches to get a stable vagrant running on 192.168.20.2 for metlog backend

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,6 @@
 Vagrant::Config.run do |config|
   config.vm.box = "centos-60-x86_64"
+  config.vm.network :hostonly, "192.168.20.2"
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "manifests"
     puppet.manifest_file  = "default.pp"
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,9 +1,14 @@
 Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/ruby/bin/' }
 
 # Mozilla packages to install
-$moz_packages = ['createrepo','nginx','gunicorn','logstash','python26','python26-setuptools','python-devel','rubygem-petef-statsd','rpm-devel','rpm-python','rpmdevtools','zeromq',]
+$moz_packages = ['createrepo','nginx','gunicorn','logstash','python26','python26-setuptools','python-devel','rubygem-petef-statsd','rpm-devel','rpm-python','rpmdevtools','zeromq','whisper','rubygem-pencil','graphite-web','carbon','django-tagging','httpd','python-twisted','mod_wsgi-3.3-1.el6.x86_64','bitmap-fonts-compat'] 
 
 yumrepo {
+    'packages-all':
+        descr       => "CentOS All",
+        baseurl     => 'http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.all',
+        enabled     => 1,
+        gpgcheck    => 0;
     'mozilla-services':
         descr       => "Mozilla Services Repo",
         baseurl     => 'http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.mozilla-services',
@@ -14,19 +19,40 @@ yumrepo {
         baseurl     => 'http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.mozilla',
         enabled     => 1,
         gpgcheck    => 0;
+    'packages-centos-base':
+        descr       => "CentOS Base",
+        baseurl     => 'http://mrepo.mozilla.org/mrepo/$releasever-$basearch/RPMS.centos-base',
+        enabled     => 1,
+        gpgcheck    => 0;
 }
 
-package { $moz_packages:
-    ensure  => present,
-    require => [Host["mrepo"], Yumrepo['mozilla-services'], Yumrepo['packages-mozilla']]
+package { 
+    $moz_packages:
+        ensure  => present,
+        require => [Host["mrepo"], 
+                    Yumrepo['packages-all'],
+                    Yumrepo['mozilla-services'],
+                    Yumrepo['packages-mozilla'],
+                    Yumrepo['packages-centos-base']];
+    'cairo-1.8.8-3.1.el6.x86_64':
+        ensure  => present,
+        require => [Host["mrepo"], 
+                    Yumrepo['packages-all']];
+    'pycairo-1.8.6-2.1.el6.x86_64':
+        ensure  => present,
+        require => [Host["mrepo"], 
+                    Package["cairo-1.8.8-3.1.el6.x86_64"],
+                    Yumrepo['packages-all']];
+
 }
 
-# From remote, one of the mrepo's doesn't work, so we hardcode in the one that
+# From remote, one of the mrepo's doesn't work, so we hardcode in the
+# one that
 # does work reliably over the VPN
 host { 'mrepo':
-    ensure  => present,
-    name    => "mrepo.mozilla.org",
-    ip      => "63.245.209.182",
+    ensure => present,
+    name => "mrepo.mozilla.org",
+    ip => "63.245.217.47",
 }
 
 ## Local RPM Repo
@@ -106,6 +132,140 @@ file {
         require => Package["zeromq"];
 }
 
+# setup the whisperdb
+file {
+    'whisperdb_init':
+        ensure => present,
+        path   => "/usr/local/bin/init_whisperdb",
+        source => "/vagrant/files/init_whisperdb",
+        owner  => "root",
+        group  => "root",
+        mode   => 755;
+}
+
+# Script to open up the firewall
+file {
+    'firewall_down':
+        ensure => present,
+        path   => "/usr/local/bin/iptables_flush",
+        source => "/vagrant/files/iptables_flush",
+        owner  => "root",
+        group  => "root",
+        mode   => 755;
+}
+
+# Graphite/Carbon config files
+file {
+    'wsgi_conf':
+        ensure => present,
+        path   => "/etc/httpd/conf.d/wsgi.conf",
+        source => "/vagrant/files/httpd/wsgi.conf",
+        owner  => "root",
+        group  => "root",
+        mode   => 644;
+    
+    "/opt/graphite/conf":
+        ensure => "directory",
+        owner  => "root",
+        group  => "root",
+        mode   => 644;
+
+    'carbon_conf':
+        ensure => present,
+        path   => "/opt/graphite/conf/carbon.conf",
+        source => "/vagrant/files/graphite/carbon.conf",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/graphite/conf"],
+        mode   => 644;
+    'storageschemas_conf':
+        ensure => present,
+        path   => "/opt/graphite/conf/storage-schemas.conf",
+        source => "/vagrant/files/graphite/storage-schemas.conf",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/graphite/conf"],
+        mode   => 644;
+    'graphite_wsgi':
+        ensure => present,
+        path   => "/opt/graphite/conf/graphite.wsgi",
+        source => "/vagrant/files/graphite/graphite.wsgi",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/graphite/conf"],
+        mode   => 755;
+}
+
+# Config files for pencil
+file {
+    '/opt/pencil':
+        ensure => "directory",
+        owner  => "root",
+        group  => "root",
+        mode   => 644;
+
+    '/opt/pencil/config':
+        ensure   => "directory",
+        owner   => "root",
+        group   => "root",
+        require => File["/opt/pencil"],
+        mode    => 644;
+
+    'pencil_graphs_yml':
+        ensure => present,
+        path   => "/opt/pencil/config/graphs.yml",
+        source => "/vagrant/files/pencil/graphs.yml",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/pencil/config"],
+        mode   => 755;
+    'pencil_dashboards_yml':
+        ensure => present,
+        path   => "/opt/pencil/config/dashboards.yml",
+        source => "/vagrant/files/pencil/dashboards.yml",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/pencil/config"],
+        mode   => 755;
+    'pencil_pencil_yml':
+        ensure => present,
+        path   => "/opt/pencil/config/pencil.yml",
+        source => "/vagrant/files/pencil/pencil.yml",
+        owner  => "root",
+        group  => "root",
+        require => File["/opt/pencil/config"],
+        mode   => 755;
+}
+
+
+## startup scripts for statsd, carbon and pencil
+file {
+    'statsd_init':
+        ensure  => present,
+        path    => "/etc/init.d/statsd",
+        source  => "/vagrant/files/startup/statsd",
+        owner   => 'root',
+        group   => 'root',
+        mode    => 755,
+        force   => true;
+    'carbon_init':
+        ensure  => present,
+        path    => "/etc/init.d/carbon",
+        source  => "/vagrant/files/startup/carbon",
+        owner   => 'root',
+        group   => 'root',
+        mode    => 755,
+        force   => true;
+    'pencil_init':
+        ensure  => present,
+        path    => "/etc/init.d/pencil",
+        source  => "/vagrant/files/startup/pencil",
+        owner   => 'root',
+        group   => 'root',
+        mode    => 755,
+        force   => true;
+}
+
 exec {
     'update_init':
         command => "/sbin/initctl reload-configuration",
@@ -118,6 +278,51 @@ exec {
         command     => "/sbin/initctl restart logstash",
         subscribe   => File["logstash.conf"],
         refreshonly => true;
+    'pencil_down':
+        command     => "/sbin/service pencil stop",
+        require     => [File["pencil_init"], 
+                        File["pencil_pencil_yml"],
+                        File["pencil_graphs_yml"],
+                        File["pencil_dashboards_yml"]];
+    'pencil_up':
+        command     => "/sbin/service pencil start",
+        require     => [File["pencil_init"], 
+                        File["pencil_pencil_yml"],
+                        File["pencil_graphs_yml"],
+                        File["pencil_dashboards_yml"]];
+    'statsd_up':
+        command     => "/sbin/service statsd start",
+        require     => File["statsd_init"];
+    'carbon_up':
+        command     => "/sbin/service carbon start",
+        require     => File["statsd_init"];
+    'iptables_down':
+        command     => "/usr/local/bin/iptables_flush",
+        require     => File["firewall_down"];
+    'init_whisperdb':
+        command     => "/usr/local/bin/init_whisperdb",
+        require     => File["whisperdb_init"];
+    'restart_apache':
+        command     => "/sbin/service httpd restart",
+        require     => [File["wsgi_conf"], Exec["iptables_down"]];
 }
 
-Host["mrepo"] -> Package["zeromq"] -> Package["logstash"] -> File["logstash.conf"] -> File["logstash_init"] -> File["logstash_plugins"] -> Exec["start_logstash"]
+
+Host["mrepo"] ->
+Package["zeromq"] ->
+Package["logstash"] ->
+File["wsgi_conf"] ->
+File["carbon_conf"] ->
+File["graphite_wsgi"] ->
+File["logstash.conf"] ->
+File["logstash_init"] ->
+File["logstash_plugins"] ->
+Exec["start_logstash"] ->
+Exec["init_whisperdb"] ->
+File["pencil_init"] ->
+File["statsd_init"] ->
+File["carbon_init"] ->
+Exec["pencil_down"] ->
+Exec["pencil_up"] ->
+Exec["iptables_down"] ->
+Exec["restart_apache"]
