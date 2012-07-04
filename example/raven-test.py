b60db919dfb791e8efad0a14d568e0852233e46f
Victor Ng <victor@crankycoder.com>
2012-07-04 14:57:39 -0400

    updated raven-test to use the default project URL

--- example/raven-test.py
+++ example/raven-test.py
@@ -1,6 +1,6 @@
 import time
 from raven import Client
-client  = Client(dsn="http://2cfcac6f616e4a90b20f4aed9f0e40dc:9a813d742a51426c9285e8222c2a65b7@192.168.20.2:9000/2")
+client  = Client(dsn="http://5030659c0a8948b8aad226cd8241e701:3b2feac77d674a3e8f094767720711d8@192.168.20.2:9000/1")
 
 try:
     1/0
