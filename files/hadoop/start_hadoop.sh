046f3bda1884befcf2a27e78fd16ae6e4da662c1
Victor Ng <victor@crankycoder.com>
2012-08-15 15:21:25 -0400

    changed the startup script to start hadoop* instead of hadoop-0.20*. We missed starting up hive by accident before

--- files/hadoop/start_hadoop.sh
+++ files/hadoop/start_hadoop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-for service in /etc/init.d/hadoop-0.20-*; do sudo $service stop; done
-for service in /etc/init.d/hadoop-0.20-*; do sudo $service start; done
+for service in /etc/init.d/hadoop*; do sudo $service stop; done
+for service in /etc/init.d/hadoop*; do sudo $service start; done
